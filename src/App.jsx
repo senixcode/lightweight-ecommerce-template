b583f221ad840ef5a3cd8dda9c76aed67b8c4068
Senix Code <codesenix@gmail.com>
2020-12-08 23:12:47 -0500

    change layout page initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, Grid } from "@material-ui/core";
 import { Navbar } from "./components/organisms/Navbar";
-
+import TestData from "./helper/TestData.json";
+import { Main } from "./components/pages/Main";
+import { Test } from "./components/pages/Test";
 export const App = () => {
   return (
     <>
       {/*CssBaseline: normalize css */}
       <CssBaseline />
-      <Navbar />
+      <Navbar search={TestData.search} />
+      <Grid container>
+        <Grid container item xs={11}>
+          {TestData.result.map((resul) => (
+            <Main key={resul.id} {...resul} />
+          ))}
+        </Grid>
+        <Grid item xs>
+          <Test></Test>
+        </Grid>
+      </Grid>
     </>
   );
 };
