8f020b0ab6af8e3b1c2759705433586d7567663f
Senix Code <codesenix@gmail.com>
2020-12-08 23:13:15 -0500

    add section category component

---
+++ src/components/pages/Test.jsx
@@ -0,0 +1,25 @@
+import {
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+} from "@material-ui/core";
+import React from "react";
+
+export const Test = () => {
+  return (
+    <div>
+      <FormLabel component="legend">Category</FormLabel>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox name="gilad" />} label="HP" />
+        <FormControlLabel control={<Checkbox name="jason" />} label="Acer" />
+        <FormControlLabel
+          control={<Checkbox name="antoine" />}
+          label="Lenovo"
+        />
+        <FormControlLabel control={<Checkbox name="antoine" />} label="ASUS" />
+        <FormControlLabel control={<Checkbox name="antoine" />} label="Apple" />
+      </FormGroup>
+    </div>
+  );
+};
