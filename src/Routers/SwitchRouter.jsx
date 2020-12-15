433e2d04809589f87e4b6322b319712013d3dbd4
Senix Code <codesenix@gmail.com>
2020-12-14 20:28:39 -0500

    add new route cart

--- src/Routers/SwitchRouter.jsx
+++ src/Routers/SwitchRouter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { Cart } from "../components/pages/Cart";
 import { Detail } from "../components/pages/Detail";
 import { Product } from "../components/pages/Product";
 
@@ -8,6 +9,7 @@ export const SwitchRouter = () => {
     <Switch>
       <Route exact path="/" component={Product} />
       <Route exact path="/product/:id" component={Detail} />
+      <Route exact path="/cart" component={Cart} />
       <Redirect to="/" />
     </Switch>
   );
