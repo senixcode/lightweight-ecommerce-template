454452e34963c9e824cea02a5b023baf8a5319c0
Senix Code <codesenix@gmail.com>
2020-12-17 02:31:28 -0500

    change type var scored

--- src/helper/TestData.js
+++ src/helper/TestData.js
@@ -3,7 +3,7 @@ const products = [
     id: "1",
     title:
       "Acer Aspire 5 Slim Laptop, 15.6 inches Full HD IPS Display, AMD Ryzen 3 3200U, Vega 3 Graphics, 4GB DDR4, 128GB SSD, Backlit Keyboard, Windows 10 in S Mode, A515-43-R19L, Silver",
-    scored: "5",
+    scored: 5,
     price: "475.00",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/81vNRgHlqGL._AC_SL1500_.jpg",
@@ -13,7 +13,7 @@ const products = [
     id: "2",
     title:
       "EVOO 14.1” Ultra Slim Notebook - Elite Series, FHD Display, AMD Ryzen 5 3500U Processor with Radeon Vega 8 Graphics, 8GB RAM, 256GB SSD, HD Webcam, Windows 10 Home, Black",
-    scored: "6",
+    scored: 4,
     price: "599.99",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/81ayQeToBhL._AC_SL1500_.jpg",
@@ -23,7 +23,7 @@ const products = [
     id: "3",
     title:
       'MICROSOFT Surface Laptop 3 - 15" - CORE I5 1035G7 - 8 GB RAM - 128 GB SSD',
-    scored: "4",
+    scored: 4.5,
     price: "1,065.00",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/61v4e6ILeZL._AC_SL1416_.jpg",
@@ -33,7 +33,7 @@ const products = [
     id: "4",
     title:
       'Acer Nitro 5 Gaming Laptop, 9th Gen Intel Core i5-9300H, NVIDIA GeForce GTX 1650, 15.6" Full HD IPS Display, 8GB DDR4, 256GB NVMe SSD, Wi-Fi 6, Backlit Keyboard, Alexa Built-in, AN515-54-5812',
-    scored: "5",
+    scored: 5,
     price: "599.99",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/71s1LRpaprL._AC_SL1500_.jpg",
@@ -43,7 +43,7 @@ const products = [
     id: "5",
     title:
       'Acer Predator Helios 300 Gaming Laptop, Intel i7-10750H, NVIDIA GeForce RTX 2060 6GB, 15.6" Full HD 144Hz 3ms IPS Display, 16GB Dual-Channel DDR4, 512GB NVMe SSD, Wi-Fi 6, RGB Keyboard, PH315-53-72XD',
-    scored: "5",
+    scored: 4.5,
     price: "1,173.09",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/71k45hZkLmL._AC_SL1500_.jpg",
@@ -53,7 +53,7 @@ const products = [
     id: "6",
     title:
       'ConceptD 3 Ezel CC314-72G-72SX Convertible Creator Laptop, Intel i7-10750H, GeForce GTX 1650 Max-Q, 14" FHD IPS, Gorilla Glass, Pantone Validated, 100% sRGB, 16GB, 512GB NVMe SSD, Wacom AES 1.0 Pen',
-    scored: "3",
+    scored: 3.5,
     price: "1,279.99",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/71450Kduk4L._AC_SL1500_.jpg",
