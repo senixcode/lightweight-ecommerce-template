a11f6a53a9972148ad4598ad22eaf6c558d0b87e
Senix Code <codesenix@gmail.com>
2020-12-17 02:35:13 -0500

    fix props description

--- src/components/molecules/CardContentCustom.jsx
+++ src/components/molecules/CardContentCustom.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { CardContent, Typography } from "@material-ui/core";
 
-export const CardContentCustom = ({ title, Description }) => {
+export const CardContentCustom = ({ title, description }) => {
   return (
     <CardContent>
       <Typography gutterBottom variant="h5" component="h2">
         {title}
       </Typography>
-      <Typography variant="body1" color="textSecondary" component="p">
-        {Description && <Description />}
+      <Typography variant="body1" color="textSecondary" component="span">
+        {description}
       </Typography>
     </CardContent>
   );
--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -2,10 +2,11 @@ import React, { useMemo } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { getProductById } from "../../helper/getProductById";
 
-import { Button, Grid } from "@material-ui/core";
+import { Box, Button, Grid } from "@material-ui/core";
 import { CardMediaCustom } from "../molecules/CardMediaCustom";
 import { CardContentCustom } from "../molecules/CardContentCustom";
 import { SectionShoppingCart } from "../organisms/SectionShoppingCart";
+import { HoverRating } from "../organisms/HoverRating";
 
 export const Detail = ({ history }) => {
   const { id } = useParams();
@@ -13,18 +14,22 @@ export const Detail = ({ history }) => {
   if (!product) {
     return <Redirect to="/" />;
   }
-  const Description = () => (
+  console.log(product);
+  const description = (
     <>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
-      quibusdam. Illo eum, eligendi odit doloremque magnam, ad totam architecto
-      aspernatur dolores quos similique perspiciatis sequi asperiores.
-      Voluptatum sapiente atque nesciunt.
+      <HoverRating scord={product.scored} />
+      <Box>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
+        quibusdam. Illo eum, eligendi odit doloremque magnam, ad totam
+        architecto aspernatur dolores quos similique perspiciatis sequi
+        asperiores. Voluptatum sapiente atque nesciunt.
+      </Box>
     </>
   );
 
   const propsCardContenCustom = {
     title: product.title,
-    Description,
+    description,
   };
   const propsCardMediaCustom = {
     alt: product.title,
