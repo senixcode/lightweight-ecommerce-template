bac10521ada9bfc37b683df31ba302195d7e04cc
Senix Code <codesenix@gmail.com>
2020-12-23 19:33:23 -0500

    decription cut functionality

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Redirect, useParams } from "react-router-dom";
 import { getProductById } from "../../helper/getProductById";
 
@@ -8,12 +8,32 @@ import { CardContentCustom } from "../molecules/CardContentCustom";
 import { SectionShoppingCart } from "../organisms/SectionShoppingCart";
 import { HoverRating } from "../organisms/HoverRating";
 import currencyFormatter from "currency-formatter";
+import { shortString } from "../../helper/shortString";
 export const Detail = ({ history }) => {
   const { id } = useParams();
+  const [slice, setSlice] = useState(true);
+  const descriptionLimit = 250;
   const product = useMemo(() => getProductById(id), [id]);
   if (!product) {
     return <Redirect to="/" />;
   }
+  const handleShowDescription = () => {
+    setSlice(!slice);
+  };
+
+  const descriptionHardCode = `
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
+        quibusdam. Illo eum, eligendi odit doloremque magnam, ad totam
+        architecto aspernatur dolores quos similique perspiciatis sequi
+        asperiores. Voluptatum sapiente atque nesciunt. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Totam aspernatur quod blanditiis
+        repudiandae facere odio distinctio debitis ducimus doloribus natus qui
+        error voluptate nemo nam, laborum voluptatibus vitae eaque minus. Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Qui, magni
+        distinctio commodi vel sit dicta asperiores iure non, eveniet impedit
+        consectetur dolore architecto suscipit reiciendis quidem nisi aliquid
+        expedita illum?
+  `;
   const description = (
     <>
       <HoverRating scord={product.scored} />
@@ -22,10 +42,12 @@ export const Detail = ({ history }) => {
         {currencyFormatter.format(product.price, { code: "USD" })}
       </Box>
       <Box style={{ marginTop: "4px" }}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit,
-        quibusdam. Illo eum, eligendi odit doloremque magnam, ad totam
-        architecto aspernatur dolores quos similique perspiciatis sequi
-        asperiores. Voluptatum sapiente atque nesciunt.
+        {slice
+          ? shortString(descriptionHardCode, descriptionLimit)
+          : descriptionHardCode}
+        <p onClick={handleShowDescription}>
+          {slice ? "show more" : " show less"}
+        </p>
       </Box>
     </>
   );
