31df5346f36d58ce2503199f8226af9c45e15393
Senix Code <codesenix@gmail.com>
2020-12-23 19:31:12 -0500

    add saved categories

--- src/components/organisms/SectionCategories.jsx
+++ src/components/organisms/SectionCategories.jsx
@@ -4,26 +4,75 @@ import {
   FormGroup,
   FormLabel,
 } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import _ from "lodash";
+export const SectionCategories = ({ filterProduct, products }) => {
+  const { get, set } = useLocalStorage();
 
-export const SectionCategories = () => {
+  let filterCategories = Array.from(
+    new Set(
+      Array.from(new Set(products.map((p) => p.category)))
+        .map((category) => {
+          let result = products.find((p) => p.category === category);
+          return result;
+        })
+        .map((r) => {
+          return { id: r.id, key: r.category, value: false };
+        })
+    )
+  );
+  let getCategories = get("categories");
+  let categoriesLocalStorage = JSON.parse(getCategories || "[]");
+  const [categories, setCategories] = useState(
+    getCategories ? categoriesLocalStorage : filterCategories
+  );
+
+  const handleChecked = (event) => {
+    let e = event.target;
+    const newCategory = {
+      key: e.name,
+      value: e.checked,
+    };
+    let getCategories = get("categories");
+    let categoriesLocalStorage = JSON.parse(getCategories || "[]");
+    const repeatedCategory = categoriesLocalStorage.find(
+      (category) => category.key === newCategory.key
+    );
+    if (repeatedCategory) {
+      let index = _.findIndex(categoriesLocalStorage, repeatedCategory);
+      repeatedCategory.value = newCategory.value;
+      let updateCategory = _.cloneDeep(repeatedCategory);
+      categoriesLocalStorage.splice(index, 1, updateCategory);
+      set("categories", categoriesLocalStorage);
+      setCategories(categoriesLocalStorage);
+    } else {
+      categoriesLocalStorage.push(newCategory);
+      set("categories", categoriesLocalStorage);
+      setCategories(categoriesLocalStorage);
+    }
+    filterProduct();
+  };
+  //console.log({ categories });
   return (
     <>
       <FormLabel component="legend">Category</FormLabel>
       <FormGroup>
-        <FormControlLabel control={<Checkbox name="gilad" />} label="HP" />
-        <FormControlLabel control={<Checkbox name="jason" />} label="Acer" />
-        <FormControlLabel
-          control={<Checkbox name="antoine" />}
-          label="Lenovo"
-        />
-        <FormControlLabel control={<Checkbox name="antoine" />} label="ASUS" />
-        <FormControlLabel control={<Checkbox name="antoine" />} label="Evoo" />
-        <FormControlLabel
-          control={<Checkbox name="antoine" />}
-          label="Surface"
-        />
-        <FormControlLabel control={<Checkbox name="antoine" />} label="Apple" />
+        {categories.map((category) => {
+          return (
+            <FormControlLabel
+              key={category.id}
+              control={
+                <Checkbox
+                  onChange={handleChecked}
+                  name={category.key}
+                  checked={category.value}
+                />
+              }
+              label={category.key}
+            />
+          );
+        })}
       </FormGroup>
     </>
   );
