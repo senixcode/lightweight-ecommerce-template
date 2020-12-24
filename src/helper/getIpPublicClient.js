6ea1c963870819e78ec5768f9c3e152f520d4b14
Senix Code <codesenix@gmail.com>
2020-12-23 22:37:19 -0500

    change consumer ip client

--- src/helper/getIpPublicClient.js
+++ src/helper/getIpPublicClient.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const getIpPublicClient = async () => {
   try {
-    const response = await Axios.get("http://icanhazip.com/");
+    const response = await Axios.get("https://ipv4.icanhazip.com/");
     return response;
   } catch (error) {
     return 400;
