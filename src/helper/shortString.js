ee729e575c435f3f405eb5030a579f4e32fc4eaf
Senix Code <codesenix@gmail.com>
2020-12-23 19:27:58 -0500

    change compressed code

--- src/helper/shortString.js
+++ src/helper/shortString.js
@@ -1,6 +1,3 @@
 export const shortString = (value, number = 70) => {
-  if (value.length > 70) {
-    return value.slice(0, number) + " ...";
-  }
-  return value;
+  return value.length > number ? value.slice(0, number) : value;
 };
